ad5ad7b016c2e050799016c7a2995f8556183b32
Mason(Yun) Chen <mason.yunchen@gmail.com>
2019-03-22 21:14:50 +1300

    correct

--- about/index.md
+++ about/index.md
@@ -3,6 +3,4 @@ layout: page
 type: about
 ---
 
-### About me
-
 I am a full stack developer interested in AI technology.
--- code/hello-jekyll_demo.cpp
+++
@@ -1,6 +0,0 @@
-class Voila {
-public:
-    // Voila
-    static const string VOILA = "Voila";
-    // will not interfere with embedded tags.
-}
